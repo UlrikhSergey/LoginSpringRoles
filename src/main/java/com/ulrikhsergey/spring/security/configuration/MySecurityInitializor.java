2d63c82d56800164183ad59468a1df02ccd85ed2
UlrikhSergey <ulrikh.sergey@gmail.com>
2022-12-06 17:59:28 +0300

    Initial commit

---
+++ src/main/java/com/ulrikhsergey/spring/security/configuration/MySecurityInitializor.java
@@ -0,0 +1,7 @@
+package com.ulrikhsergey.spring.security.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MySecurityInitializor  extends
+        AbstractSecurityWebApplicationInitializer {
+}
